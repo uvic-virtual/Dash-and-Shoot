a481850464ab91c76db8c5a15c1cfb6175c3897a
Alex Li <alex116.dll@gmail.com>
2020-12-07 03:40:18 -0800

    added steamvr and making main scene

---
+++ Dash and Shoot/Assets/Dash-and-Shoot/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Manages game states
+/// Manages game settings
+/// </summary>
+public class PlayerManager: MonoBehaviour
+{
+    [Header("Players")]
+    [SerializeField] private GameObject _playerVR;
+    [SerializeField] private GameObject _playerPC;
+    [SerializeField] private GameObject _playerAndroid;
+
+    void Start()
+    {
+        SpawnPlayer();
+    }
+
+    private void SpawnPlayer()
+    {
+        Debug.Log("Current plateform ==>" + Application.platform);
+        if (UnityEngine.XR.XRDevice.isPresent)
+        {
+            Instantiate(_playerVR, Vector3.zero, Quaternion.identity);
+        }
+        else if (Application.platform == RuntimePlatform.Android)
+        {
+            Instantiate(_playerAndroid, Vector3.zero, Quaternion.identity);
+        }
+        else if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            Instantiate(_playerPC, Vector3.zero, Quaternion.identity);
+        }
+    }
+
+}
